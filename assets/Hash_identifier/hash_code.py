1a710ffb6b0b3d68c3d1b539dc3563ddfd057430
CyberChef-Aidoo <nana038mail@gmail.com>
2026-08-08 23:06:57 +0000

    hashCode-init

---
+++ assets/Hash_identifier/hash_code.py
@@ -0,0 +1,11 @@
+# This is a simple CMD line too to check the 
+# hash type of a given hash string. It uses regex to identify the hash type based on its length and character set. 
+
+
+
+import argparse
+import re
+import sys
+import os
+
+
