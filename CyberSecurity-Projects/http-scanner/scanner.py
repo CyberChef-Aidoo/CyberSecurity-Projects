94c067c06541f93a3081d77d4323270b6097c3f1
CyberChef-Aidoo <nana038mail@gmail.com>
2026-08-16 03:16:43 +0000

    Feat: Add comments for clarity in scanner.py

--- CyberSecurity-Projects/http-scanner/scanner.py
+++ CyberSecurity-Projects/http-scanner/scanner.py
@@ -1,9 +1,18 @@
+#This is the package for 
 import argparse
+#This is for regular expression
 import re
+#This is for system interaction
 import sys
+#This is for data classes default boilaterplate
 from dataclasses import dataclass
+#This is for typing hints.
 from typing import Literal
+#this is for making HTTP requests.
 import httpx
+#this is for rich text formatting or colorful output in the terminal.
 from rich.console import Console
+#this is for creating panels in the terminal output.
 from rich.panel import Panel
+#This is for creating tables in the terminal output.
 from rich.table import Table
